0138d19ffa2113341adfdc1633e825d8692da6e8
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-06-02 20:50:38 +0500

    Update 05_strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -15,10 +15,22 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
+/*
+Key characteristics of Substring:
+
+If either startIndex or endIndex is less than 0 or greater than the string's length, they are treated as if they were 0 and the string's length, respectively.
+If startIndex is greater than endIndex, the method swaps the two arguments, effectively making substring(startIndex, endIndex) equivalent to substring(endIndex, startIndex).
+
+*/
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
+/*Key characteristics of Slice:
+
+If startIndex is negative, it is treated as string.length + startIndex.
+If endIndex is negative, it is treated as string.length + endIndex.
+If startIndex is greater than endIndex, slice returns an empty string.*/
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
