9fd28045356afaa15a59d2deae2d6ee2ba133f6f
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-06-03 17:31:35 +0500

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -23,13 +23,23 @@ const JsUser = {
 // console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
+
+// We can freeze the object via using below syntax so that no on can change it 
 // Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
+//Function are first citizen in the js mean we can treat them as a variable
+
+// it will return function call back mean just the reference, and it will not not execute the function
+
+// console.log(myObj.greetings)
+
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+
+// in any object we want use its another property via this
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
