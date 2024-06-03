1e6567e0bcaffc4b3e5669bbff6d83d4300c3c1a
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-06-03 13:55:19 +0500

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -1,6 +1,8 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
+
+// it will create 2D array and it will treat dc_heros as a item, its best alternative is concat() function , it doesnot modify original it  returns new one array
 // marvel_heros.push(dc_heros)
 
 // console.log(marvel_heros);
@@ -9,6 +11,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
+// one more way that is Spread Operator
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
