c4fdd0d171b05369844218fdff959160b6fe58c1
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-06-03 12:12:25 +0500

    Update 01_arrays.js

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,18 +1,27 @@
 // array
-
+// first way
 const myArr = [0, 1, 2, 3, 4, 5]
 const myHeors = ["shaktiman", "naagraj"]
-
+// second way 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
 
 // Array methods
 
+// push is used to insert one or more elemnt in the array but in last 
+
 // myArr.push(6)
+// myArr.push(500,600,600)
 // myArr.push(7)
+
+// pop is used to remove the last element from the array 
 // myArr.pop()
 
+
+// unshift is used to insert  one or more elemnet in the start of the array
 // myArr.unshift(9)
+
+// shift is used to remove the elemnet from the start of the array
 // myArr.shift()
 
 // console.log(myArr.includes(9));
