f621c4da6b6b0d88edcfd6871d1a54140e0c8df9
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-06-03 14:10:38 +0500

    Update 02_array.js

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -25,10 +25,10 @@ console.log(real_another_array);
 
 console.log(Array.isArray("Hitesh"))
 console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.from({name: "hitesh"})) // interesting for interview Prespective
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
-
+// we can pass any type of element to create an array
 console.log(Array.of(score1, score2, score3));
